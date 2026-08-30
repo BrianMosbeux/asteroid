c9eae4bcaab7db250479e26b617f453e6de67ca1
Brian Mosbeux <b.mosbeux@gmail.com>
2026-08-30 15:00:21 +0200

    put Player class in updatable and drawable group and use the groups to draw and update player in the game loop

--- main.py
+++ main.py
@@ -11,18 +11,20 @@ def main():
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT /2
-    player = Player(x, y)
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT /2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
