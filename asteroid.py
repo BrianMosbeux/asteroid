e8338fd55c55fc7c4f5df55d30778e856c86a95c
Brian Mosbeux <b.mosbeux@gmail.com>
2026-09-01 16:14:39 +0200

    feat: split asteroid when shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -11,6 +13,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(a=20, b=30)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        velocity = self.velocity * 1.2
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = velocity.rotate(angle) 
+        asteroid_2.velocity = velocity.rotate(-angle) 
+
+        
+
 
 
 
