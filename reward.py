6956247cf8b740bb8359199c0889ddebc22f41ca
Brian Mosbeux <b.mosbeux@gmail.com>
2026-09-09 14:09:58 +0200

    add duration to scrap rewards

--- reward.py
+++ reward.py
@@ -7,10 +7,15 @@ class Scrap(CircleShape):
     def __init__(self, x: float, y: float):
         super().__init__(x, y, SCRAP_RADIUS)
         self.value = 1
+        self.cooldown = 5
 
     def draw(self, screen):
         pygame.draw.rect(surface=screen, color="white", rect=pygame.Rect(self.position.x-self.radius, self.position.y-self.radius, self.radius*2, self.radius*2), width=LINE_WIDTH)
 
     def update(self, dt):
-        return
+        self.cooldown -= dt
+        if self.cooldown > 0:
+            return
+        self.kill()
+
 
