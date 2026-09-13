f3e7769c741e6ec911c2d3734966c095a45fa9c5
Brian Mosbeux <b.mosbeux@gmail.com>
2026-09-13 10:50:25 +0200

    rename reward cooldown to countdown and add visual hint of countdown expiring

--- reward.py
+++ reward.py
@@ -7,14 +7,15 @@ class Scrap(CircleShape):
     def __init__(self, x: float, y: float):
         super().__init__(x, y, SCRAP_RADIUS)
         self.value = 1
-        self.cooldown = 5
+        self.countdown = 5
 
     def draw(self, screen):
         pygame.draw.rect(surface=screen, color="white", rect=pygame.Rect(self.position.x-self.radius, self.position.y-self.radius, self.radius*2, self.radius*2), width=LINE_WIDTH)
 
     def update(self, dt):
-        self.cooldown -= dt
-        if self.cooldown > 0:
+        self.countdown -= dt
+        if self.countdown > 0:
+            self.radius -= dt
             return
         self.kill()
 
